a52cafd084b5fb1c0eb851d675d9f2ff8e763f0a
mohammad mazaheri <53862625+xmsa@users.noreply.github.com>
2019-10-09 13:49:50 +0330

    code 1 set size and location panels

--- manpass/main.cs
+++ manpass/main.cs
@@ -15,6 +15,21 @@ namespace manpass
         public frm_Main()
         {
             InitializeComponent();
+            //set location 
+            panel_Add_Edit_View.Location = new Point(71, 64);
+            panel_Help.Location = new Point(71, 64);
+            panel_Login.Location = new Point(71, 64);
+            panel_Manager.Location = new Point(71, 64);
+            panel_Setting.Location = new Point(71, 64);
+            panel_SignUp.Location = new Point(71, 64);
+            //set size
+            panel_Add_Edit_View.Size = new Size(715, 453);
+            panel_Help.Size = new Size(715, 453);
+            panel_Login.Size = new Size(715, 453);
+            panel_Manager.Size = new Size(715, 453);
+            panel_Setting.Size = new Size(715, 453);
+            panel_SignUp.Size = new Size(715, 453);
+
         }
     }
 }
